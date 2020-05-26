7888faff353bf0a2eaa8ed124c6738c0792aa9ca
Sean Barton <linglither@gmail.com>
2020-05-26 09:39:13 -0600

    Initial Commit

---
+++ SeanCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SeanCard
+//
+//  Created by Sean Barton on 2020-05-26.
+//  Copyright © 2020 Sean Barton. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
